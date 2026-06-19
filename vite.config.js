4c2fddbe41682b704adb456f3974403bf75dcfe6
Purrnanssh Sinha <prrnnssh@gmail.com>
2026-06-19 20:53:33 +0530

    Fix Vite base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 // The user specified the final URL should be https://purrnanssh.github.io//
 // This means the base path is root.
 export default defineConfig({
-  base: '/',
+  base: '/Portfolio/',
 })
