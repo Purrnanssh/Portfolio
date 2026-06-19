60447545b02951ea200c8efb0744328fbdccb1c6
Purrnanssh Sinha <prrnnssh@gmail.com>
2026-06-19 20:48:54 +0530

    Configure Vite and gh-pages for deployment

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+// The user specified the final URL should be https://purrnanssh.github.io//
+// This means the base path is root.
+export default defineConfig({
+  base: '/',
+})
